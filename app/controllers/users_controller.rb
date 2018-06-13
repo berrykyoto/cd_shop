15376ba7733c24b5d4fce69e9076f4b290825d2e
takuya <tak10.ti19@gmail.com>
2018-06-13 04:08:21 +0000

    add controller modified admin user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+		@orders = @user.orders.page(params[:page])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+     	if @user.update(user_params)
+        	redirect_to user_path(@user.id)
+    	else
+      		render :edit
+    	end
+	end
+
+	def destroy
+		user = User.find(params[:id])
+  		user.destroy
+  		redirect_to items_path #redirect先変える
+  	end
+
+private
+  	def user_params
+      	params.require(:user).permit(:name, :name_kana, :email, :post_code, :address, :phone, :password)
+  	end
+end
