092d0e5e1e8b3f8ff403e7950b8f11f8d4ff6aa8
Naoki Yano <vegetables412@gmail.com>
2018-06-17 06:37:38 +0000

    itemのnew

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,14 @@ has_many :records, inverse_of: :Item
 accepts_nested_attributes_for :records, allow_destroy: true
 has_many :cart_items
 has_many :order_items
-def self.search(search) 
-    if search 
+def self.search(search)
+    if search
       Item.where(['title LIKE ?', "%#{search}%"])
     else
       Item.all
     end
-  end
+end
+
+enum style: { single: 0, album: 1 }
+
 end
