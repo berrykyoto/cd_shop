0d9f3c5e08711f36d0b9625a8dd3a49dfbb657ff
Naiharu <rsrsnmlol@gmail.com>
2018-06-17 13:01:51 +0900

    一覧と詳細変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,15 @@
 class Item < ApplicationRecord
 belongs_to :admin
 belongs_to :genre
-has_many :records
+has_many :records, inverse_of: :Item
+accepts_nested_attributes_for :records, allow_destroy: true
 has_many :cart_items
 has_many :order_items
+def self.search(search) 
+    if search 
+      Item.where(['title LIKE ?', "%#{search}%"])
+    else
+      Item.all
+    end
+  end
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,8 @@
 class Record < ApplicationRecord
-belongs_to :item
-has_many :songs
+belongs_to :item, inverse_of: :records
+has_many :songs, inverse_of: :records
+accepts_nested_attributes_for :songs, allow_destroy: true
+
+validates :item, presence: true
+
 end
