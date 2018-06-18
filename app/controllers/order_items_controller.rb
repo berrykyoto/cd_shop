69b5443eb7d6bd9e68dc4c294674f4148ee572dc
saitoumizuki <yoshihiramizuki@gmail.com>
2018-06-18 12:51:35 +0900

    orders変更

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,6 +3,11 @@ def new
 	@order_item = OrderItem.new
 	@cart_items = current_user.cart_items.all.includes(:item)
 	@order = Order.find(params[:id])
+
+	@total_price = 0
+		@cart_items.each do |cart_item|
+			@total_price = @total_price + cart_item.item.price * cart_item.quantity
+	end
 end
 
 def create
