81a2b6f35ae2f8d70df850edbf0fd08272f3814b
saitoumizuki <yoshihiramizuki@gmail.com>
2018-06-19 13:03:07 +0900

    orders変更

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,8 +1,10 @@
 class OrderItemsController < ApplicationController
 def new
 	@order_item = OrderItem.new
-	@cart_items = current_user.cart_items.all.includes(:item)
-	@order = Order.find(params[:id])
+	@cart_items = current_user.cart_item.all.includes(:item)
+	@orders = Order.all
+	@order = current_user.@orders.last
+	# 前ページでクリエイトした送付先情報などを取得（カレントユーザーに紐づいたorders（複数）のうち最後（最新）のorder）
 
 	@total_price = 0
 		@cart_items.each do |cart_item|
@@ -12,6 +14,12 @@ end
 
 def create
 	@order_item = OrderItem.new(order_item_params)
+	if @order_item.save
+		flash[:notice] = '注文が完了しました'
+        redirect_to items_path　# トップページに遷移
+    else
+    	render :new
+    end
 end
 
 end
