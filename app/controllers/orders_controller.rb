887928211b4dc69d62da61932b0df81117fe7c5a
saitoumizuki <yoshihiramizuki@gmail.com>
2018-06-21 16:48:31 +0900

    注文ができるようになりました

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,25 +1,10 @@
 class OrdersController < ApplicationController
 	def index
 		@orders = Order.all.includes(:user)
-		@order_items = OrderItem.all
-		# オーダーアイテムを全件取得
-		@first_item = @order_items.first
-		# オーダーアイテム（複数）のうち、先頭の商品名を取得
-
-		@total_price = 0
-		# オーダーの合計金額（sub_priceの合計）を計算する。
 	end
 
 	def show
 		@order = Order.find(params[:id])
-		@order_items = OrderItem.all
-		# 各オーダーごとのオーダーアイテムを全件取得
-
-		@total_price = 0
-			@order_items.each do |order_item|
-				@total_price = @total_price + order_item.sub_price
-		# オーダーの合計金額（sub_priceの合計）を計算する。
-		end
 	end
 
 	def new
@@ -30,7 +15,6 @@ class OrdersController < ApplicationController
 	def create
 		@order = Order.new(order_params)
 		 @order.save
-			flash[:notice] = 'オーダーが保存できたよ(*^-^*)このメッセージは後で消してね'
         	redirect_to new_order_item_path
     	# else
     	# 	render :new
@@ -39,24 +23,22 @@ class OrdersController < ApplicationController
 
 	def edit
 		@order = Order.find(params[:id])
-		@order_items = OrderItem.all
-		# 各オーダーごとのオーダーアイテムを全件取得
-
-		@total_price = 0
-			@order_items.each do |order_item|
-				@total_price = @total_price + order_item.sub_price
-		# オーダーの合計金額（sub_priceの合計）を計算する。
-		end
 	end
 
 	def update
 		@order = Order.find(params[:id])
-		if order.update(order_params)
+		if @order.update(order_params)
        		redirect_to orders_path
     	else
      		render :edit
     	end
 	end
+
+	def destroy
+		order = Order.find(params[:id])
+  		order.destroy
+  		redirect_to orders_path
+  	end
 
 private
   	def order_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180618050322) do
+ActiveRecord::Schema.define(version: 20180621023920) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20180618050322) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "sub_price"
+    t.integer "total_price"
   end
 
   create_table "orders", force: :cascade do |t|
